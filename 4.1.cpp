6641073463e9bd4f3dcf6a0bf4a6f00caac38400
Diego Rodríguez Romero <110944222+di3g0r@users.noreply.github.com>
2023-11-02 18:56:00 +0000

    tercer avance

--- 4.1.cpp
+++ 4.1.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <stdlib.h> 
 #include <vector>
+#include <time.h>
 
 using namespace std;
 /*
@@ -34,8 +35,10 @@ class Graph{
             numEdges = E;
 
             adjLists = new list<int> [V];
-            adjMatrix = new  vector<vector<int>> [V]; 
+
+            adjMatrix = new vector<vector<int>> [V]; 
             adjMatrix->resize(V,vector<int>(V, 0));
+            
             visited = new bool[V];
 
         }
@@ -56,6 +59,8 @@ class Graph{
 };
 
 void Graph::printGraph(){
+    //adjList
+    std::cout << "Adjacency List\n"; 
     for(int i = 0; i < this->numVertices; i++){
         std::cout << "\nVertex " << i << ":";
 
@@ -64,13 +69,26 @@ void Graph::printGraph(){
         }
         std::cout << "\n";
     }
+    std::cout << "\n";
+    //adjMatrix
+    std::cout << "Adjacency Matrix\n\n";
+    int n = 0;
+    for (auto k: this->adjMatrix[n]){
+        for(int j = 0; j < this->numVertices; j++){
+            std::cout << k[j];
+        }
+        std::cout << "\n";
+        n++;
+    }
+    std::cout << "\n";
 }
 
 void Graph::addEdge(int s, int d){
     this->adjLists[s].push_back(d);
     this->adjLists[d].push_back(s);
 
-    this->adjMatrix[s][d] = 1;
+    this->adjMatrix->at(s)[d] = 1;
+    this->adjMatrix->at(d)[s] = 1;
 
 }
 
@@ -120,35 +138,40 @@ void Graph::BFS(int startVertex){
 }
 
 void Graph::LoadGraph(int n, int m){
-
+    srand (time(NULL));
+    //Graph(n, m);
     //Lenamos la adjList de forma random
     int cont = 0;
-    int conexiones = rand() % m + 1;
-    int vertices = rand() % n + 1;
     while(cont < m){
-        int conexion = rand() % m + 1;
+        int conexion = rand() % n + 1;
         int vertice = rand() % n + 1;
-        addEdge(vertice, conexion);
-        cont += 1;
+        if(this->adjMatrix->at(vertice)[conexion] == 0 && conexion != vertice){
+            addEdge(vertice, conexion);
+            cont += 1;
+        }
     }
-
-    //LLenamos la adjMatrix de forma random
 }
 
 int main(){
-    Graph G(5);
+    /*
+    Graph G(5, 3);
 
     G.addEdge(0, 1);
     G.addEdge(0, 2);
     G.addEdge(0, 3);
     G.addEdge(1, 2);
     G.addEdge(0, 4);
-
+    
     G.printGraph();
+    */
 
     //G.DFS(0);
 
-    G.BFS(0);
+    //G.BFS(0);
+
+    Graph G(6, 5);
+    G.LoadGraph(6, 5);
+    G.printGraph();
 
     return 0;
 }
